a785a90c751e0c7ed469f442efb784f498257eda
Tominsin <olasenitosin@gmail.com>
2022-09-13 16:45:57 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Check if it is a sign
+ * @n: parameter for the function
+ *
+ * Return: 1 and + if n is greater than zero
+ * 0 if n is zero
+ * -1 and - f n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < '0')
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
