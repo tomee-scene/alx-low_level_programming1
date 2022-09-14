afc4eb1640a520eedfb63ff1afdbf6b887529a16
Tominsin <olasenitosin@gmail.com>
2022-09-13 20:07:01 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * add two integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: Returns an int
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = int a + int b;
+
+	return (sum);
+}
