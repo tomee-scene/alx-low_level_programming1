8d2bfd8ed1c2f5a546e82e6081389db3a6db0b21
Tominsin <olasenitosin@gmail.com>
2022-09-22 10:36:04 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes string to uppercase
+ *
+ * @str: string
+ * Return: return
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 'a' + 'A';
+		i++;
+	}
+	return (str);
+}
