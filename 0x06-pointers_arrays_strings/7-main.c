14cd608ecdfffe46a45f86b231ab12d2fd43d406
Tominsin <olasenitosin@gmail.com>
2022-09-22 19:41:33 -0700

    magic

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -4,16 +4,11 @@
 int main(void)
 {
 	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-
-	    char *p;
-
-
-
-	        p = leet(s);
-
-		    printf("%s", p);
-
-		        printf("%s", s);
-
-			    return (0);
+	char *p;
+	
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
+	
+	return (0);
 }
