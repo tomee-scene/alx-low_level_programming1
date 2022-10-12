77d37d32f320bd800e4b76991a74436c75b66412
Tominsin <olasenitosin@gmail.com>
2022-10-12 13:57:32 -0700

    Whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - function to print name
@@ -10,6 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
