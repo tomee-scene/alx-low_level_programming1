df38a27dadcf3a4e513974a06b7aa1e03d0dfc4b
Tominsin <olasenitosin@gmail.com>
2022-11-18 11:02:49 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,14 +17,14 @@ int len(char *str)
 }
 
 /**
- * strcpy - copies the string pointed to by src,
+ * _strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest
  * @dest: copy source to this buffer
  * @src: this is the source to copy
  * Return: copy of original source
  */
-char *strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
@@ -61,7 +61,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog1);
 			return (NULL);
 		}
-		dog1->name = strcpy(copy_of_name, name);
+		dog1->name = _strcpy(copy_of_name, name);
 	}
 	else
 		dog1->name = NULL;
@@ -75,7 +75,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog1);
 			return (NULL);
 		}
-		dog1->owner = strcpy(copy_of_owner, owner);
+		dog1->owner = _strcpy(copy_of_owner, owner);
 	}
 	else
 		dog1->owner = NULL;
